21caeecb7510f97d585890ac9857b43a62e05cca
Fate <sahmanish10987@gmail.com>
2022-04-08 20:48:12 +0530

    A c++ code is added -Fate Averruncus

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int num=11;
+	for(int i=0; i<num; i++){
+		cout<<i<<"->";
+	}
+	cout<<"End of list\n";
+	cout<<"Code Successfully executed\n";
+	return 0;
+}
